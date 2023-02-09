c38d152a41e62a8c8db812402643029862596f5e
Julio Sahuquillo <jusahu@edem.es>
2023-02-09 12:40:25 +0100

    Update generator.py
    Actualización del generador para adaptarlo a una oficina (Marina y Julio)

--- 01_Generador/generator.py
+++ 01_Generador/generator.py
@@ -45,7 +45,7 @@ class PubSubMessages:
         self.publisher.transport.close()
         logging.info("PubSub Client closed.")
             
-lista_devices = ["TV", "horno", "microondas", "nevera", "lavadora"]
+lista_devices = ["TV", "aire acondicionado", "microondas", "cafetera", "ordenador", "lampara"]
 
 #Generator Code
 def generateMockData(client_id, device_id, name, kw, hour):
@@ -98,23 +98,25 @@ def run_generator(project_id, topic_name):
             for client_id in clients:
                     for device in clients[client_id]:
                         kw = "0"
-                        if device[1] == "TV" and hour in [14,15,16,17,18,19,20,21,22]:
+                        if device[1] == "TV" and hour in [9,10,11,12,13,15,16,17,18]:
                             kw = random.uniform(0.40, 0.80)
-                        elif device[1] == "horno" and hour in [14,15,16,17,18,19,20,21,22]:
-                            kw = random.uniform(1.20,1.40) 
-                        elif device[1] == "microondas" and hour in [14,15,16,17,18,19,20,21,22]:
+                        elif device[1] == "aire acondicionado" and hour in [9,10,11,12,13,14,15,16,17,18]:
+                            kw = random.uniform(1.32,1.98) 
+                        elif device[1] == "microondas" and hour in [14]:
                             kw = random.uniform(1.00, 1.50)
-                        elif device[1] == "lavadora" and hour in [14,15,16,17,18,19,20,21,22]:
-                            kw = random.uniform(1.80, 2.20)
-                        elif device[1] == "nevera" and hour in [14,15,16,17,18,19,20,21,22]:
-                            kw = str(random.uniform(0.48, 0.78))
+                        elif device[1] == "cafetera" and hour in [9,11,15]:
+                            kw = random.uniform(0.72, 0.90)
+                        elif device[1] == "ordenador" and hour in [9,10,11,12,13,15,16,17,18]:
+                            kw = str(random.uniform(0.20, 0.30))
+                        elif device[1] == "lampara" and hour in [9,10,11,12,13,14,15,16,17,18]:
+                            kw = str(random.uniform(0.01, 0.015))
                         else:
-                            kw = str(random.uniform(0.01,0.05))
+                            kw = str(random.uniform(0.001,0.005))
                         message = generateMockData(client_id, device[0], device[1], kw, str(start))
                         print(message)
                         pubsub_class.publishMessages(message)
                         #it will be generated a transaction each 2 seconds
-                        #time.sleep(1))
+                        #time.sleep(1)
     except Exception as err:
         logging.error("Error while inserting data into out PubSub Topic: %s", err)
     finally:
