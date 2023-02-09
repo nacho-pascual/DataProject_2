192962acb105f85594d0cd7007bd352daac56e4f
Julio Sahuquillo <113711092+jusahu@users.noreply.github.com>
2023-02-09 12:45:20 +0100

    Maju generador clientes 4 (#17)
    * update generator.py
    
    Julio y yo hemos estado mejorando el generador intentando definir mejor el rango de consumo de cada device cogiendo datos reales de consumo.
    
    * update generator.py again
    
    Cambio de lugar el bucle, porque antes estaba en la inicialización y siempre daba el mismo valor.
    
    * Update generator.py again
    
    Me había olvidado un espacio en el cálculo de KW del horno.
    
    * update generator
    
    Hemos solucionado el error a la hora de generar los datos dentro del rango que queremos
    
    * MaJu ataca de nuevo
    
    Marina y Julio mejorando la generación de datos para tener el consumo de dispositivos según horas. No está acabado, pero necesitamos lanzar cosas desde el ordenador de Julio. Seguiremos informando...
    
    * MaJu al rescate
    
    Marina y yo vamos generando poco a poco datos con sentido, si seguimos así yo creo que en mayo lo finiquitamos.
    
    * Update generator.py
    
    Actualización del generador para adaptarlo a una oficina (Marina y Julio)
    
    ---------
    
    Co-authored-by: Marina Pérez <marina1611@gmail.com>
    Co-authored-by: Julio Sahuquillo <jusahu@edem.es>
    Co-authored-by: Marina Pérez <113711058+marinapb16@users.noreply.github.com>

--- 01_Generador/generator.py
+++ 01_Generador/generator.py
@@ -10,6 +10,7 @@ from faker import Faker
 from datetime import datetime
 import pytz
 from google.cloud import pubsub_v1
+from datetime import datetime, timezone, timedelta
 
 fake = Faker()
 
@@ -44,11 +45,11 @@ class PubSubMessages:
         self.publisher.transport.close()
         logging.info("PubSub Client closed.")
             
-lista_devices = ["TV", "horno", "microondas", "nevera", "lavadora"]
+lista_devices = ["TV", "aire acondicionado", "microondas", "cafetera", "ordenador", "lampara"]
 
 
 #Generator Code
-def generateMockData(client_id, device_id, name, kw):
+def generateMockData(client_id, device_id, name, kw, hour):
 
 
     #Return values
@@ -57,8 +58,8 @@ def generateMockData(client_id, device_id, name, kw):
         "device_id": device_id,
         "client_id": client_id,
         "device_name": name,
-        "kw": kw,
-        "timestamp": str(datetime.utcnow())
+        "kw": kw
+        "timestamp": hour 
     }
 
 
@@ -78,27 +79,46 @@ def run_generator(project_id, topic_name):
         for n in range (0, num_devices):
             device_id = str(uuid.uuid4())
             clients[client_id].append((device_id, lista_devices[n]))
-
-    print(clients)          
+    print(clients)
     try:
-        while True:
+        # En febrero empiezan las recopilación de datos de un a casa
+        start = datetime(2023, 2, 1, tzinfo=timezone.utc)
+        # El 28 de febrero dejamos de tomar datos
+        end = datetime(2023, 2, 28, tzinfo=timezone.utc)
+        # Suponemos que tenemos una visita cada 5 segundos
+        delta_datos = timedelta(seconds=5)
+
+        while start < end:
+            start += delta_datos
+            # def franja_horaria(str_timestamp):
+            #     dt = datetime.strptime(str_timestamp, '%Y-%m-%d %H:%M:%S.%f')
+            #     # Convertir de UTC a tiempo local
+            #     dt = dt.replace(tzinfo=pytz.timezone('Europe/Madrid')).astimezone()
+            hour = start.hour
+
             for client_id in clients:
-                 for device in clients[client_id]:
-                    if device[1] == "TV":
-                        kw = random.uniform(0.40, 0.80)
-                    elif device[1] == "horno":
-                        kw = random.uniform(1.20, 1.40)
-                    elif device[1] == "microondas":
-                        kw = random.uniform(1.00, 1.50)
-                    elif device[1] == "nevera":
-                        kw = random.uniform(0.48, 0.78)
-                    elif device[1] == "lavadora":
-                        kw = random.uniform(1.80, 2.20)
-                    message = generateMockData(client_id, device[0], device[1], kw)
-                    print(message)
-                    pubsub_class.publishMessages(message)
-                    #it will be generated a transaction each 2 seconds
-                    time.sleep(5)
+                    for device in clients[client_id]:
+                        kw = "0"
+                        if device[1] == "TV" and hour in [9,10,11,12,13,15,16,17,18]:
+                            kw = random.uniform(0.40, 0.80)
+                        elif device[1] == "aire acondicionado" and hour in [9,10,11,12,13,14,15,16,17,18]:
+                            kw = random.uniform(1.32,1.98) 
+                        elif device[1] == "microondas" and hour in [14]:
+                            kw = random.uniform(1.00, 1.50)
+                        elif device[1] == "cafetera" and hour in [9,11,15]:
+                            kw = random.uniform(0.72, 0.90)
+                        elif device[1] == "ordenador" and hour in [9,10,11,12,13,15,16,17,18]:
+                            kw = str(random.uniform(0.20, 0.30))
+                        elif device[1] == "lampara" and hour in [9,10,11,12,13,14,15,16,17,18]:
+                            kw = str(random.uniform(0.01, 0.015))
+                        else:
+                            kw = str(random.uniform(0.001,0.005))
+                        message = generateMockData(client_id, device[0], device[1], kw, str(start))
+                        print(message)
+                        pubsub_class.publishMessages(message)
+                        #it will be generated a transaction each 2 seconds
+                        #time.sleep(1)
+
     except Exception as err:
         logging.error("Error while inserting data into out PubSub Topic: %s", err)
     finally:
