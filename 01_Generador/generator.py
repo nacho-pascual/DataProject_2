cd45ed82ab91813a715052b004ecb189324072e0
Marina Pérez <113711058+marinapb16@users.noreply.github.com>
2023-02-10 12:35:50 +0100

    maju al rescate (#18)
    * maju al rescate
    
    importante porque faltba una coma y daba error
    
    * update
    
    * Update generator AGAIN
    
    Venga que ahora sí que es la última (o no). MaJu.
    
    * Redondear KW a 3 decimales
    
    * Redondear KW a 3 decimales y limpiar código
    
    ---------
    
    Co-authored-by: Julio Sahuquillo <jusahu@edem.es>
    Co-authored-by: Victor Ruiz <vruizext@gmail.com>

--- 01_Generador/generator.py
+++ 01_Generador/generator.py
@@ -5,10 +5,7 @@ import uuid
 import random
 import logging
 import argparse
-import google.auth
 from faker import Faker
-from datetime import datetime
-import pytz
 from google.cloud import pubsub_v1
 from datetime import datetime, timezone, timedelta
 
@@ -39,7 +36,7 @@ class PubSubMessages:
         json_str = json.dumps(message)
         topic_path = self.publisher.topic_path(self.project_id, self.topic_name)
         publish_future = self.publisher.publish(topic_path, json_str.encode("utf-8"))
-        logging.info("A New transaction has been registered. Id: %s", message)
+        logging.info("A new measure has been sent: %s", message)
 
     def __exit__(self):
         self.publisher.transport.close()
@@ -58,7 +55,7 @@ def generateMockData(client_id, device_id, name, kw, hour):
         "device_id": device_id,
         "client_id": client_id,
         "device_name": name,
-        "kw": kw
+        "kw": kw,
         "timestamp": hour 
     }
 
@@ -81,19 +78,15 @@ def run_generator(project_id, topic_name):
             clients[client_id].append((device_id, lista_devices[n]))
     print(clients)
     try:
-        # En febrero empiezan las recopilación de datos de un a casa
+        # En febrero empiezan las recopilación de datos
         start = datetime(2023, 2, 1, tzinfo=timezone.utc)
         # El 28 de febrero dejamos de tomar datos
         end = datetime(2023, 2, 28, tzinfo=timezone.utc)
-        # Suponemos que tenemos una visita cada 5 segundos
+        # Los dispositivos mandan datos cada 5 segundos
         delta_datos = timedelta(seconds=5)
 
         while start < end:
             start += delta_datos
-            # def franja_horaria(str_timestamp):
-            #     dt = datetime.strptime(str_timestamp, '%Y-%m-%d %H:%M:%S.%f')
-            #     # Convertir de UTC a tiempo local
-            #     dt = dt.replace(tzinfo=pytz.timezone('Europe/Madrid')).astimezone()
             hour = start.hour
 
             for client_id in clients:
@@ -101,23 +94,30 @@ def run_generator(project_id, topic_name):
                         kw = "0"
                         if device[1] == "TV" and hour in [9,10,11,12,13,15,16,17,18]:
                             kw = random.uniform(0.40, 0.80)
+                        elif device[1] == "TV" and hour in [19] and start.day == 2:
+                            kw = random.uniform(0.40,0.80) 
                         elif device[1] == "aire acondicionado" and hour in [9,10,11,12,13,14,15,16,17,18]:
-                            kw = random.uniform(1.32,1.98) 
+                            kw = random.uniform(1.32, 1.98) 
+                        elif device[1] == "aire acondicionado" and hour in [19] and start.day == 4:
+                            kw = random.uniform(1.32, 1.98) 
                         elif device[1] == "microondas" and hour in [14]:
-                            kw = random.uniform(1.00, 1.50)
+                            kw = random.uniform(1.00, 1.50) 
                         elif device[1] == "cafetera" and hour in [9,11,15]:
                             kw = random.uniform(0.72, 0.90)
                         elif device[1] == "ordenador" and hour in [9,10,11,12,13,15,16,17,18]:
-                            kw = str(random.uniform(0.20, 0.30))
+                            kw = random.uniform(0.20, 0.30)
+                        elif device[1] == "ordenador" and hour in [19,20,21] and start.day == 6:
+                            kw = random.uniform(0.20, 0.30)
                         elif device[1] == "lampara" and hour in [9,10,11,12,13,14,15,16,17,18]:
-                            kw = str(random.uniform(0.01, 0.015))
+                            kw = random.uniform(0.01, 0.015)
+                        elif device[1] == "lampara" and hour in [19,20,21,22,23,0,1,2,3,4,5,6,7,8] and start.day == 7:
+                            kw = random.uniform(0.01, 0.015)
                         else:
-                            kw = str(random.uniform(0.001,0.005))
-                        message = generateMockData(client_id, device[0], device[1], kw, str(start))
+                            kw = random.uniform(0.001,0.005)
+                        message = generateMockData(client_id, device[0], device[1], str(round(kw, 3)), str(start))
                         print(message)
                         pubsub_class.publishMessages(message)
-                        #it will be generated a transaction each 2 seconds
-                        #time.sleep(1)
+                        time.sleep(1)
 
     except Exception as err:
         logging.error("Error while inserting data into out PubSub Topic: %s", err)
